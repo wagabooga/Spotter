338f9a845795d462b6bd41cf58642de3fa7d3438
wagabooga <mattanthu@hotmail.com>
2021-11-19 20:55:00 -0800

    save

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,31 @@
+// ------------------------- require -------------------------
+// require("dotenv").config();
+
+// ------------------------- Web server config -------------------------
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8000
+// const cookieParser = require("cookie-parser"); // Note: (npm install cookieparser)
+
+// ------------------------- PG database client/connection setup -------------------------
+// const { Pool } = require("pg");
+// const dbParams = require("./lib/db.js");
+// const db = new Pool(dbParams);
+// db.connect();
+
+
+// ------------------------- require routes (resources) -------------------------
+const indexRoutes = require("./routes/templates/index");
+
+
+// ------------------------- use routes -------------------------
+app.use("/api/favourites", favouriteAPIRoutes(db));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// Start Server
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
