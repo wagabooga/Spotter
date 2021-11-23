2f32166337dc4a9a8aa8f74e359b2b778ff70d53
Sarah <sarahejohnson89@gmail.com>
2021-11-23 11:06:54 -0800

    update button color to green and add onclick event to console

--- frontend/src/components/homepage/MiddlePostButton.js
+++ frontend/src/components/homepage/MiddlePostButton.js
@@ -1,11 +1,30 @@
 import * as React from "react";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  btn: {
+    backgroundColor: "#1DB954",
+    "&:hover": {
+      backgroundColor: "green",
+    },
+    color: "white",
+  },
+});
 
 export default function BasicButtons() {
+  const classes = useStyles();
+
   return (
     <Stack spacing={2} direction="row" justifyContent="end">
-      <Button variant="text">Post</Button>
+      <Button
+        onClick={() => console.log("you clicked me")}
+        className={classes.btn}
+        variant="text"
+      >
+        Post
+      </Button>
       {/* <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button> */}
     </Stack>
