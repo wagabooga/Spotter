7fb8dc5d07947860aa1827ecb2e4a48ca0846c2f
Sarah <sarahejohnson89@gmail.com>
2021-11-25 14:36:39 -0800

    update landing page layout to include material UI layout, and login with spotify button. Also update them to root folder so that typography is the same throughout the site. Also updated the colour of the Home page. Will need to update some other colours on home ppage as Material UI has overwritten them.  Will do on a separate branch for home page once I have access to newly made spot containers from Matt

--- frontend/src/components/landing/LandingPage.js
+++ frontend/src/components/landing/LandingPage.js
@@ -1,24 +1,42 @@
-import React from 'react'
-import "../../styles/landingPage.css"
-import SpotifyButton from './SpotifyButton'
+// import React from "react";
+// import "../../styles/landingPage.css";
+import SpotifyButton from "./SpotifyButton";
+
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+const Item = styled(Box)(({ theme }) => ({
+  ...theme.typography.body1,
+  padding: theme.spacing(1),
+  textAlign: "beginning",
+  color: theme.palette.text.secondary,
+}));
 
 export default function LandingPage() {
   return (
-    <div className="landing-outer-container">
-
-      <div className="landing-left-container">
-          <img src="https://abs.twimg.com/sticky/illustrations/lohp_en_1302x955.png" alt="alternate_text" width="width" height="height">
-          </img>
-      </div>
-
-      <div>
-        <h1>Happening now</h1>
-        <h3>Join Spotter Today.</h3>
-        <div>
-          <SpotifyButton />
-        </div>
-      </div>
-
-    </div>
-  )
+    <Box sx={{ flexGrow: 2 }}>
+      <Grid container spacing={0} columns={16}>
+        <Grid item xs={10}>
+          <Item>
+            <img
+              src="https://abs.twimg.com/sticky/illustrations/lohp_en_1302x955.png"
+              alt="alternate_text"
+              width="100%"
+              height="100%"
+            ></img>
+          </Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>
+            <h1>Happening now</h1>
+            <h3>Join Spotter Today.</h3>
+            <SpotifyButton />
+          </Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
--- frontend/src/components/landing/SpotifyButton.js
+++ frontend/src/components/landing/SpotifyButton.js
@@ -1,5 +1,23 @@
 // import React, { Fragment, useEffect } from 'react';
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 import axios from "axios";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  Spotifybtn: {
+    direction: "row",
+    justifyContent: "center",
+    backgroundColor: "#ccedcd",
+    color: "black",
+    "&:hover": {
+      backgroundColor: "dark grey",
+      color: "#1DB954",
+    },
+    // width: "45px",
+  },
+});
 
 export default function SpotifyButton() {
   //loginHandler works by first making a get request, which /login/redirect returns (res.send) client_id and url_body, and then manually refreshes the page with spotify's AUTH_URL
@@ -7,16 +25,32 @@ export default function SpotifyButton() {
     axios({
       method: "get",
       url: "http://localhost:8000/login/redirect",
-    }).then((response) => {
-      window.location.replace(`https://accounts.spotify.com/authorize?client_id=${response.data.client_id}${response.data.url_body}`)
-      return `https://accounts.spotify.com/authorize?client_id=${response.data.client_id}${response.data.url_body}`
     })
-    .catch((err) => { console.log("myresponse:", err) })
-  }
-  
+      .then((response) => {
+        window.location.replace(
+          `https://accounts.spotify.com/authorize?client_id=${response.data.client_id}${response.data.url_body}`
+        );
+        return `https://accounts.spotify.com/authorize?client_id=${response.data.client_id}${response.data.url_body}`;
+      })
+      .catch((err) => {
+        console.log("myresponse:", err);
+      });
+  };
+
+  const classes = useStyles();
   return (
     // throw the method on the button's onClick
-    <button className="landing-right-container" onClick={spotifyLoginHandler}> Login with Spotify</button>
-    )
-  }
-  
+
+    <Stack spacing={2} direction="row">
+      <Button
+        variant="outlined"
+        className={classes.Spotifybtn}
+        onClick={spotifyLoginHandler}
+      >
+        Login with Spotify
+      </Button>
+    </Stack>
+
+    // <button className="landing-right-container" onClick={spotifyLoginHandler}> Login with Spotify</button>
+  );
+}
