87d22df4fe152e03d1f67495453cfa715cf86a98
wagabooga <mattanthu@hotmail.com>
2021-11-30 00:01:08 -0800

    Changed seeds for presentation

--- db/seeds/02_spots.sql
+++ db/seeds/02_spots.sql
@@ -1,13 +1,31 @@
 INSERT INTO spots (user_id, spot_text, date_created, is_respot, spotify_json) VALUES  
-(1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel interdum nisi. Quisque laoreet pharetra tortor eu ultrices. Nunc ut.', '2021-11-21', FALSE, '{
-"albumUrl": "https://i.scdn.co/image/ab67616d0000485197fdd7cf1b14d78c6a7fb17f",
-"artist": "Masked Wolf",
-"title": "Astronaut In The Ocean",
-"uri": "spotify:track:3Ofmpyhv5UAQ70mENzB277"
-}'),
-(2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel interdum nisi. Quisque laoreet pharetra tortor eu ultrices. Nunc ut.', '2021-11-21', FALSE, '{
-"albumUrl": "https://i.scdn.co/image/ab67616d0000485197fdd7cf1b14d78c6a7fb17f",
-"artist": "Masked Wolf",
-"title": "Astronaut In The Ocean",
-"uri": "spotify:track:3Ofmpyhv5UAQ70mENzB277"
-}')
+(1, 'Stack banana til the mornin come!', '2021-11-21',  FALSE, '{
+  "artist":"Harry Belafonte",
+  "title":"Banana Boat (Day-O)",
+  "uri":"spotify:track:4fHDlIntTsRGSyTg5UYZYC",
+  "albumUrl":"https://i.scdn.co/image/ab67616d00004851d46dec4689741b1400c3df95"}'),
+
+(1, 'Had so much fun at Karaoke last night belting this absolute banger', '2021-11-21', FALSE, '{
+  "artist":"Queen",
+  "title":"Bohemian Rhapsody",
+  "uri":"spotify:track:6l8GvAyoUZwWDgF1e4822w",
+  "albumUrl":"https://i.scdn.co/image/ab67616d000048515a7602f0e508660b2ba40f8c"}'),
+  
+(1, 'How many times can I get away with playing this', '2021-11-21', FALSE, '{
+  "artist":"Mariah Carey",
+  "title":"All I Want for Christmas Is You",
+  "uri":"spotify:track:0bYg9bo50gSsH3LtXe2SQn",
+  "albumUrl":"https://i.scdn.co/image/ab67616d000048514246e3158421f5abb75abc4f"}'),
+
+(1, 'Justin Bieber makes me smile! Like this if you smiled 😊 ', '2021-11-21', FALSE, '{
+  "artist":"Justin Bieber",
+  "title":"U Smile",
+  "uri":"spotify:track:6Xw2FLih8m5ItDbyP992HH",
+  "albumUrl":"https://i.scdn.co/image/ab67616d00004851629dc9e2e3bc20bbd7d92e4a"}'),
+  
+
+(1, 'Love the artwork from this album, check out the bees! 🐝🌻', '2021-11-21', FALSE, '{
+  "artist":"Tyler, The Creator",
+  "title":"See You Again (feat. Kali Uchis)",
+  "uri":"spotify:track:7KA4W4McWYRpgf0fWsJZWB",
+  "albumUrl":"https://i.scdn.co/image/ab67616d000048518940ac99f49e44f59e6f7fb3"}')
